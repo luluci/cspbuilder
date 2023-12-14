6ac0c71caf668ea84e07362fdc580720cba14532
luluci <lunacied@gmail.com>
2023-12-14 21:12:55 +0900

    node-modulesを最新に更新

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,8 @@
 import * as vscode from 'vscode';
 import { CSPBuilderPanel } from './CSPBuilderView';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -31,5 +31,5 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 }
 
-// this method is called when your extension is deactivated
+// This method is called when your extension is deactivated
 export function deactivate() {}
